9ac1396d84063eab393fd3f8c236f521c5fe4afc
ekobilal042 <ekobilal042@gmail.com>
2023-11-26 22:44:25 +0700

    update router

--- router.js
+++ router.js
@@ -1,11 +1,13 @@
- // File: router.js
+// File: router.js
 
 'use strict';
 
 const controller = require('./controller');
 
 module.exports = function (app) {
-    app.route('/api/data').get(controller.getAllData);
+    app.route('/api/data').get((req, res) => {
+        console.log('Accessing /api/data');
+        controller.getAllData(req, res);
+    });
     // Anda dapat menambahkan rute lainnya di sini
 };
-
