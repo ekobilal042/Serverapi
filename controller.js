24facd84bf939a81196363e0d6d6e6c18339f843
ekobilal042 <ekobilal042@gmail.com>
2023-11-26 22:18:01 +0700

    kontroler

---
+++ controller.js
@@ -0,0 +1,21 @@
+ // File: controller.js
+
+'use strict';
+
+const response = require('./res');
+const connection = require('./koneksi');
+
+module.exports = {
+    getAllData: function (req, res) {
+        connection.query('SELECT * FROM nama_tabel', function (error, rows, fields) {
+            if (error) {
+                console.error('Error querying database:', error);
+                response.ok('Error querying database', res);
+            } else {
+                response.ok(rows, res);
+            }
+        });
+    }
+    // Anda dapat menambahkan fungsi kontroler lainnya di sini
+};
+
