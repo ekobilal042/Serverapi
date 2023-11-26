21ef710a204a8e95fa8b9674c2098addb5d82300
ekobilal042 <ekobilal042@gmail.com>
2023-11-26 22:14:21 +0700

    file rest

---
+++ res.js
@@ -0,0 +1,13 @@
+ // File: res.js
+
+'use strict';
+
+module.exports.ok = function (values, res) {
+    const data = {
+        'status': 200,
+        'values': values
+    };
+    res.json(data);
+    res.end();
+};
+
