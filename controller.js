72272ac6ec2b74cf27da71575e03b05ee497c656
ekobilal042 <ekobilal042@gmail.com>
2023-11-26 22:37:54 +0700

    kontrol baru lagi

--- controller.js
+++ controller.js
@@ -1,4 +1,4 @@
- // File: controller.js
+// File: controller.js
 
 'use strict';
 
@@ -6,16 +6,25 @@ const response = require('./res');
 const connection = require('./koneksi');
 
 module.exports = {
-    getAllData: function (req, res) {
-        connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
-            if (error) {
-                console.error('Error querying database:', error);
-                response.ok('Error querying database', res);
-            } else {
-                response.ok(rows, res);
-            }
-        });
+    getAllData: function(req, res) {
+        try {
+            connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+                if (error) {
+                    console.error('Error querying database:', error);
+                    response.ok('Error querying database', res);
+                } else {
+                    if (rows && rows.length > 0) {
+                        response.ok(rows, res);
+                    } else {
+                        console.log('No data found in the database.');
+                        response.ok('No data found', res);
+                    }
+                }
+            });
+        } catch (error) {
+            console.error('Error in getAllData:', error);
+            response.ok('Error in getAllData', res);
+        }
     }
     // Anda dapat menambahkan fungsi kontroler lainnya di sini
 };
-
